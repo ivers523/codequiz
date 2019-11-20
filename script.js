e783c7aeaf4ad0da0bdebfffc6aecae6829013b1
Michael Iverson <michaeliverson@Michaels-MBP-6.domain>
2019-11-19 18:54:32 -0600

    Implemented function for generating buttons

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
 console.log("script running");
 
-// declare page elements
+// declare page elements and variables
 const timerEl = document.getElementById("timer");
 const startButtonEl = document.getElementById("start-button");
 const buttonOneEl = document.getElementById("button1");
 const buttonTwoEl = document.getElementById("button2");
 const buttonThreeEl = document.getElementById("button3");
 const buttonFourEl = document.getElementById("button4");
+const qTitle = document.getElementById("title");
+const input = document
 
 document.getElementById("question-container").style.display = "none";
 
-function startQuiz () {
-    document.getElementById("question-container").style.display = "block"; 
-    document.getElementById("start-container").style.display = "none";
-    console.log("start button")
-    
-}
-
-
 // initialize quiz on the click of the "Start Quiz" button
-// Render questions which consists of Question (text) and 4 buttons with responses
-// create arrays for questions. each question will have an array, and then there will be a larger "master array" consisting of all of the questions
+
+function startQuiz() {
+    document.getElementById("question-container").style.display = "block";
+    document.getElementById("start-container").style.display = "none";
+
+// render the question page
+    function renderQuestion () {
+        for (i = 0; i < questions.length; i++) {
+          qTitle.innerHTML = (questions[i].title);
+          buttonOneEl.innerHTML = (questions[i].choices[0]);
+          buttonTwoEl.innerHTML = (questions[i].choices[1]);
+          buttonThreeEl.innerHTML = (questions[i].choices[2]);
+          buttonFourEl.innerHTML = (questions[i].choices[3]);
+          const 
+
+          }
+
+          
+            
+
+          }
+
+        
+    
+        
+    }
+renderQuestion();
+}   
+
+    
+//randomly pull question title and choices into the html elements (h4, buttons)
+
